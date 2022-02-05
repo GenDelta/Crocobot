a2d8124910efdc4b353dccfc5fa392f1972c8cba
GenDelta <ankush.dutta.kol@gmail.com>
2022-02-05 21:02:49 +0530

    Update

--- commands/test/rolesselector.js
+++ commands/test/rolesselector.js
@@ -3,15 +3,15 @@ const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js")
 module.exports = {
     name: "rolesselector",
     category: "test",
-    devOnly: true,
+    devOnly: false,
     run: async ({client, message, args}) => {
         message.channel.send({
             embeds: [
-                new MessageEmbed().setTitle("Select Role").setDescription("Select roles from the buttons below").setColor("BLUE")
+                new MessageEmbed().setTitle("Select Role").setDescription("Select roles from the buttons below").setColor("BLUE"),
             ],
             components: [
                 new MessageActionRow().addComponents([
-                    new MessageButton().setCustomId("role-935243920177242122").setStyle("PRIMARY").setLabel("5")
+                    new MessageButton().setCustomId("role-931946323253538857").setStyle("PRIMARY").setLabel("OTHER")
                 ])
             ]
         })
